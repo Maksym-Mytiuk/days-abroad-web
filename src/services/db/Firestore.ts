c42f303b67b085bc551a8b78707c6b23135d0c5c
Maksym-Mytiuk <m.mytiuk@gmail.com>
2023-09-07 22:14:10 +0200

    fix: ci

--- src/services/db/Firestore.ts
+++ src/services/db/Firestore.ts
@@ -8,7 +8,7 @@ import {
   DocumentData,
   PartialWithFieldValue,
 } from 'firebase/firestore';
-import { firebaseConfig } from '../../../env';
+import { firebaseConfig } from '../../utils/firebaseConfig';
 
 const FIREBASE_APP = initializeApp(firebaseConfig);
 
