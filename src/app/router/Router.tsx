82cba1691998194287f104e4ef09955336ee17b1
Maksym-Mytiuk <m.mytiuk@gmail.com>
2024-02-11 13:33:29 +0100

    fix: wip

--- src/app/router/Router.tsx
+++ src/app/router/Router.tsx
@@ -9,7 +9,7 @@ const Home = React.lazy(() => import('@/features/home'));
 const Account = React.lazy(() => import('@/features/account'));
 const Trips = React.lazy(() => import('@/features/trips'));
 const Statistic = React.lazy(() => import('@/features/statistic'));
-const SignIn = React.lazy(() => import('@/features/SignIn'));
+const SignIn = React.lazy(() => import('@/features/sign-in'));
 const NotFound = React.lazy(() => import('@/features/notFound'));
 
 export const ROUTES = {
